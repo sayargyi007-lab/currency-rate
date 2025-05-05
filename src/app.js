dd482cdb8ef3b037adb7a1f8a7262cb98715310d
SayarGyi007-lab <your-thss152021@gmail.com>
2025-05-05 15:27:18 +0700

    server change

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ import route from "./routes/rate.js"
 
 const app = express()
 
-app.use(cors({
-    origin:process.env.ORIGIN,
-    credentials: true
+const allowedOrigins = [process.env.ORIGIN, 'http://localhost:5173'];
 
-}))
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
